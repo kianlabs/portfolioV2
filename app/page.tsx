ce3744218d2bbf9945913ca6552cbc4ebd37100b
ridzkyan0504-arch <ridzkyan0504@gmail.com>
2026-08-31 19:35:23 +0700

    feat: add floating WhatsApp button that hides near contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "../components/About";
 import CTA from "../components/CTA";
 import FAQ from "../components/FAQ";
+import FloatingWhatsApp from "../components/FloatingWhatsApp";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
@@ -30,6 +31,7 @@ export default function Home() {
       </main>
 
       <Footer />
+      <FloatingWhatsApp />
     </>
   );
 }
